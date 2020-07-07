114cdc4611937590d4d573026e51eb8b12645352
FUJIWARA Shunichiro <fujiwara.shunichiro@gmail.com>
2020-07-07 16:45:52 +0900

    update

--- README.md
+++ README.md
@@ -2,7 +2,20 @@
 
 SQL query runner for JSONL.
 
+[![GoDoc](https://godoc.org/github.com/fujiwara/go-jsqlite/validator?status.svg)](https://godoc.org/github.com/fujiwara/go-jsqlite/validator)
+
+jsqlite uses [go-sqlite3](https://github.com/mattn/go-sqlite3) internal.
+
+## Usage
+
+### jsqlite command
+
+```console
+$ jsqlite "SELECT tag, time, message FROM records WHERE message LIKE '%error%' AND tag LIKE 'foo.%'" tests/logs.json
+{"message":"[ERROR] hoge","tag":"foo.bar","time":"2020-07-06T17:49:37+0900"}
+{"message":"[ERROR] hoge baz","tag":"foo.baz","time":"2020-07-06T17:49:43+0900"}
+```
+
 ## LICENSE
 
 MIT
-
--- cmd/jsqlite/main.go
+++ cmd/jsqlite/main.go
@@ -2,21 +2,54 @@ package main
 
 import (
 	"encoding/json"
-	"log"
+	"flag"
+	"fmt"
+	"io"
 	"os"
 
 	"github.com/fujiwara/go-jsqlite"
 )
 
 func main() {
-	q := os.Args[1]
-	runner, _ := jsqlite.Read(os.Stdin)
-	rows, err := runner.Select(q)
+	err := run()
 	if err != nil {
-		log.Fatal(err)
+		fmt.Fprintln(os.Stderr, err.Error())
+		os.Exit(1)
+	}
+}
+
+func run() error {
+	flag.Parse()
+	if len(os.Args) < 1 {
+		flag.Usage()
+		return nil
+	}
+	query := os.Args[1]
+	var f io.Reader
+	if len(os.Args) > 2 {
+		var err error
+		f, err = os.Open(os.Args[2])
+		if err != nil {
+			return err
+		}
+	} else {
+		f = os.Stdin
+	}
+
+	runner, err := jsqlite.Read(f)
+	if err != nil {
+		return err
+	}
+	rows, err := runner.Select(query)
+	if err != nil && !jsqlite.NoSuchColumnError(err) {
+		return err
+	}
+	if len(rows) == 0 {
+		return nil
 	}
 	enc := json.NewEncoder(os.Stdout)
 	for _, row := range rows {
 		enc.Encode(row)
 	}
+	return nil
 }
