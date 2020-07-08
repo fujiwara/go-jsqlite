b0af36c71f30860e614c43e70455deaffa7eb317
FUJIWARA Shunichiro <fujiwara.shunichiro@gmail.com>
2020-07-08 11:08:30 +0900

    performance tuning
    wrapped src with bufio.Reader
    SQLite prepared statement
    etc.

--- query.go
+++ query.go
@@ -1,6 +1,7 @@
 package jsqlite
 
 import (
+	"bufio"
 	"encoding/json"
 	"fmt"
 	"io"
@@ -18,6 +19,7 @@ const (
 var (
 	tableName    = "records"
 	placeHolders []string
+	bufSize      = 64 * 1024
 )
 
 func init() {
@@ -28,8 +30,11 @@ func init() {
 
 // QueryRunner represents a query runner of jsqlite.
 type QueryRunner struct {
-	db   *sqlx.DB
-	cols map[string]struct{}
+	db        *sqlx.DB
+	cols      []string
+	colsSet   map[string]struct{}
+	colsDef   string
+	stmtCache map[string]*sqlx.Stmt
 }
 
 // Table returns a SQLite table name.
@@ -66,8 +71,9 @@ func New(dsn string) (*QueryRunner, error) {
 // NewWithDB creates a QueryRunner with the *sqlx.DB
 func NewWithDB(db *sqlx.DB) *QueryRunner {
 	return &QueryRunner{
-		db:   db,
-		cols: make(map[string]struct{}),
+		db:        db,
+		colsSet:   make(map[string]struct{}),
+		stmtCache: make(map[string]*sqlx.Stmt),
 	}
 }
 
@@ -82,12 +88,20 @@ func Read(r io.Reader) (*QueryRunner, error) {
 
 // Read reads JSONL via io.Reader, creates table on in-memory SQLite and inserts records.
 func (r *QueryRunner) Read(src io.Reader) error {
+	switch src.(type) {
+	case *bufio.Reader:
+	default:
+		src = bufio.NewReaderSize(src, bufSize)
+	}
 	dec := json.NewDecoder(src)
-
 	tx := r.db.MustBegin()
 	defer tx.Rollback()
+	defer func() {
+		r.stmtCache = make(map[string]*sqlx.Stmt)
+	}()
+	var row map[string]interface{}
 	for {
-		row := make(map[string]interface{}, 100)
+		row = make(map[string]interface{}, 100)
 		if err := dec.Decode(&row); err != nil {
 			if err == io.EOF {
 				break
@@ -114,35 +128,31 @@ func (r *QueryRunner) manageTable(tx *sqlx.Tx, row map[string]interface{}) error
 		return nil
 	}
 
-	addCols := make([]string, 0)
+	adds := make([]string, 0)
 	for col := range row {
-		if _, ok := r.cols[col]; !ok {
-			addCols = append(addCols, col)
-			r.cols[col] = struct{}{}
+		if _, exists := r.colsSet[col]; exists {
+			continue
 		}
+		adds = append(adds, col)
 	}
-	if len(addCols) == len(r.cols) {
-		if err := r.createTable(tx); err != nil {
+	if len(adds) == 0 {
+		return nil
+	}
+	if len(r.cols) == 0 {
+		if err := r.createTable(tx, adds); err != nil {
 			return err
 		}
-	} else if len(addCols) > 0 {
-		if err := r.alterTable(tx, addCols); err != nil {
+	} else {
+		if err := r.alterTable(tx, adds); err != nil {
 			return err
 		}
 	}
 	return nil
 }
 
-func (r *QueryRunner) createTable(tx *sqlx.Tx) error {
-	var colDefs []string
-	for col := range r.cols {
-		colDefs = append(colDefs, `"`+col+`"`)
-	}
-	q := fmt.Sprintf(
-		`CREATE TABLE %s(%s)`,
-		r.Table(),
-		strings.Join(colDefs, ", "),
-	)
+func (r *QueryRunner) createTable(tx *sqlx.Tx, cols []string) error {
+	r.addCols(cols...)
+	q := fmt.Sprintf(`CREATE TABLE %s(%s)`, r.Table(), r.colsDef)
 	if _, err := tx.Exec(q); err != nil {
 		return err
 	}
@@ -159,14 +169,26 @@ func (r *QueryRunner) alterTable(tx *sqlx.Tx, cols []string) error {
 		if _, err := tx.Exec(q); err != nil {
 			return err
 		}
+		r.addCols(col)
 	}
 	return nil
 }
 
+func (r *QueryRunner) addCols(cols ...string) {
+	for _, col := range cols {
+		r.cols = append(r.cols, col)
+		r.colsSet[col] = struct{}{}
+		if r.colsDef != "" {
+			r.colsDef += ","
+		}
+		r.colsDef += `"` + col + `"`
+	}
+}
+
 func (r *QueryRunner) insert(tx *sqlx.Tx, row map[string]interface{}) error {
-	values := make([]interface{}, 0, len(row)/2)
-	var colDefs []string
-	for col, v := range row {
+	values := make([]interface{}, 0, len(r.cols))
+	for _, col := range r.cols {
+		v := row[col]
 		switch v.(type) {
 		case nil, string, float64, bool:
 			values = append(values, v)
@@ -175,14 +197,29 @@ func (r *QueryRunner) insert(tx *sqlx.Tx, row map[string]interface{}) error {
 			b, _ := json.Marshal(v)
 			values = append(values, string(b))
 		}
-		colDefs = append(colDefs, `"`+col+`"`)
 	}
 	q := fmt.Sprintf(
 		"INSERT INTO %s(%s) VALUES (%s)",
 		r.Table(),
-		strings.Join(colDefs, ","),
-		strings.Join(placeHolders[0:len(colDefs)], ","),
+		r.colsDef,
+		strings.Join(placeHolders[0:len(values)], ","),
 	)
-	_, err := tx.Exec(q, values...)
-	return err
+	if stmt, err := r.prepare(tx, q); err != nil {
+		return err
+	} else {
+		_, err := stmt.Exec(values...)
+		return err
+	}
+}
+
+func (r *QueryRunner) prepare(tx *sqlx.Tx, query string) (*sqlx.Stmt, error) {
+	if stmt, exists := r.stmtCache[query]; exists {
+		return stmt, nil
+	}
+	if stmt, err := tx.Preparex(query); err != nil {
+		return nil, err
+	} else {
+		r.stmtCache[query] = stmt
+		return stmt, nil
+	}
 }
